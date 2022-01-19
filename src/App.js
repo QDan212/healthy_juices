1bf8fb2f148bf5232fdca84c09fe7fb0379ff44c
Quincy Daniels <QUINCY.DANIELS@student.mec.cuny.edu>
2022-01-19 16:19:38 -0500

    made changes to Smiley.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Smiley from './Components/Smiley';
 
 function App() {
+
+  const JuiceCards = [
+    {
+      title: "Kiwi and Banana Sea-moss",
+      prep_time: "10 mins",
+      image: "https://nutritiontwins.com/wp-content/uploads/2018/08/banana-kiwi-smoothie.jpg" ,
+      alt_text: "Best Way to take Kiwi",
+      ingredients: ["Kiwi","Banana","Sea-moss"],
+      
+    },
+
+
+    {
+      title: "Prineapple and Mango Sea-moss",
+      prep_time: "18 mins",
+      image: "https://nutritiontwins.com/wp-content/uploads/2018/08/banana-kiwi-smoothie.jpg" ,
+      alt_text: "Best Way to take Mango",
+      ingredients: ["Mango","Pineapple","Sea-moss"],
+      
+    },
+
+
+    {
+      title: "Sea-moss Infor.",
+      prep_time: "3 days",
+      image: "https://nutritiontwins.com/wp-content/uploads/2018/08/banana-kiwi-smoothie.jpg" ,
+      alt_text: "Best Way to take Sea-Moss",
+      ingredients: ["Sea-moss", "Spring Water"],
+      
+    }
+
+  ]
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JuiceCards.map(
+        (oneCard) => 
+        {
+          return (
+            <Smiley juice={oneCard.title} />
+            
+          )
+        })
+      }
+      
     </div>
+
   );
 }
 
--- src/Components/Smiley.js
+++ src/Components/Smiley.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import Rating from '@mui/material/Rating';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
-export default function Smiley() {
-  return <div></div>;
+export default function Smiley({
+    juice,
+    healthBenefits,
+    seaMossInfoPage,
+    image
+})
+{
+    
+  return(
+     <Card>
+        {/* <CardMedia
+		    component='img'
+		    height='140'
+		    image= "https://nutritiontwins.com/wp-content/uploads/2018/08/banana-kiwi-smoothie.jpg"
+            alt="This is my first react project"
+	    /> */}
+       <CardContent>
+           <h2>Page 1</h2>
+           <h2>
+		        {juice}
+		    </h2>
+       </CardContent>
+       {/* <CardContent>
+		    <h2>
+		        {juice}
+		    </h2>
+		    <p>
+		        {healthBenefits}
+		    </p>
+		    <ol>
+		        {.map(
+			// .map is a built in function of arrays that allows you to process them one at a time.
+			// remember when using .map that you need to provide a *unique* key attribute for each item
+			    (ingredient) => {
+			     return <li key={ingredient}>{ingredient}</li>;}
+		    )}
+		    </ol>
+		    <Rating name="no-value" value={null} />
+	    </CardContent> */}
+
+    </Card>
+  )
+
+
 }
